dba48fb96aae1130f28e4618517233de16f1af66
Mirko Raimondo Aiello <aiello@doneref.com>
2025-09-15 08:46:35 +0200

    Add docstring demonstration for Persona class

---
+++ 5 classi/21_docstrings.py
@@ -0,0 +1,20 @@
+"""Esempio di utilizzo delle DocStrings in una classe semplice."""
+
+
+class Persona:
+    """Rappresenta una persona generica."""
+
+    def __init__(self, nome, eta):
+        """Inizializza la persona con un nome e un'et\u00e0."""
+        self.nome = nome
+        self.eta = eta
+
+    def saluta(self):
+        """Restituisce un messaggio di saluto."""
+        return f"Ciao, sono {self.nome} e ho {self.eta} anni."
+
+
+if __name__ == "__main__":
+    print("DocString del modulo:\n", __doc__)
+    print("\nDocString della classe Persona:\n", Persona.__doc__)
+    print("\nDocString del metodo saluta:\n", Persona.saluta.__doc__)
